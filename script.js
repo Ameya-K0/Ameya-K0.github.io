18dd0e3805833474a3b563c9cb1c8bffd8ee72e9
Ameya-K0 <ameyakumthekar@gmail.com>
2025-02-03 23:49:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("navbar-container").innerHTML = data;
         });
 });
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = slides.length - 1; }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+// Auto-slide every 5 seconds (optional)
+setInterval(() => { plusSlides(1); }, 5000);
